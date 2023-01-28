e44e2053f583e53599d73651e0e6fa4fe1d51e4e
Michał Niedbalski <michnie507@student.polsl.pl>
2023-01-28 22:24:55 +0100

    Negation to be done and thats all

--- GaussSeidelASM/GaussSeidelASM.asm
+++ GaussSeidelASM/GaussSeidelASM.asm
@@ -19,9 +19,11 @@ MyProc1 proc
             movss   xmm1, dword ptr[rsp+40]    ;Storing tolerance value in xmm1 register
             xor     r14, r14                   ;Initialize k loop counter in r14 register
     mainLoop: 
+            cmp     r13b, 1
+            je      endOfCalc
             mov     r13b, 1                    ;Initialize boolean variable done in r13b register
             xor     r15, r15                   ;Initialize j loop counter in r15 register with value 0
-    jLoop:                       
+    jLoop:                 
             movss   xmm2, dword ptr[r9+r15]    ;Moving temp value to xmm2 register
             shufps  xmm1, xmm1, 93h            ;XMM1 = [][][tolerance][]
             movss   xmm1, xmm2                 ;XMM1 = [][][tolerance][temp]
@@ -42,30 +44,32 @@ MyProc1 proc
             imul    r10, r15                   ;
             add     r10, r15                   ;Getting address equations[j][j] = r8+(r15*r15+r15)*4 
             imul    r10, 4                     ;
-            add     r10, r8                    ;
-            movss   xmm2, dword ptr[r10]       ;XMM2 = [][][][equations[j][j]]
+            movss   xmm2, dword ptr[r8+r10]    ;XMM2 = [][][][equations[j][j]]
             divss   xmm0, xmm2                 ;XMM0 = [equations[j][k]][][][x[j]] 
             shufps  xmm1, xmm1, 93h            ;XMM1 = [][tolerance][temp][x[k]]
             movss   xmm1, xmm0                 ;XMM1 = [][tolerance][temp][x[j]]
-            pextrd  dword ptr [r10], xmm0, 0   ;Saving x[j]=sum/equations[j][j] value in memory
+            pextrd  dword ptr [r9+r15*4], xmm0, 0   ;Saving x[j]=sum/equations[j][j] value in memory
             movss   xmm0, xmm1                 ;XMM0 = [equations[j][k]][][][x[j]] MAYBE NOT NEEDED, BUT IDK IF pextrd FULLY EXTRACTS OR NOT
 
             xorps   xmm2, xmm2                 ;Clearing for use in comparing to 0
+            shufps  xmm1, xmm1, 39h            ;XMM1 = [x[j]][][tolerance][temp]
             subss   xmm0, xmm1                 ;x[j]-temp
-            cmpltss xmm0, xmm2                 ; remember that value of x[j] is overwritten by True or False
+            comiss  xmm0, xmm2                 ;remember that value of x[j] is overwritten by True or False
             jl      Negative      ;jl          ;jump if x[j]-temp<0
+    AbsDone:
             shufps  xmm1, xmm1, 39h            ;XMM1 = [temp][x[k]][][tolerance]
-            cmpltss xmm0, xmm1                 ; remember that value of x[j] is overwritten by True or False
+            comiss  xmm0, xmm1                 ;remember that value of x[j] is overwritten by True or False
             jg      doneFalse  ;jg             ;if x[j]-temp > tolerance
-            jmp     jLoop
     incrJ: 
-            add     r15, 1                     ;Incrementing j counter
-            cmp     r15, rdx                   ;Checking if loop has finished (loops until j==n)
-            je      endOfCalc                  ;If j==n, jump to end
+            inc     r15                        ;Incrementing j counter
+            cmp     r15, rcx                   ;Checking if loop has finished (loops until j==n)
+            je      mainLoop                   ;If j==n, jump back to mainLoop
             jmp     jLoop
     Negative:
-            ;;mulss   xmm0, -1                 ;ABS[xmm0[0]]#######################################
-
+            
+            movss xmm0, 0xbf800000
+            jmp AbsDone
+            
     doneFalse:
             mov     r13b,0                     ;done=false
             jmp     jLoop
@@ -91,12 +95,10 @@ MyProc1 proc
             subss   xmm0, xmm1                 ;sum=sum-equations[j][k]*x[k]
             shufps  xmm1, xmm1,39h             ;XMM1 = [x[k]][][tolerance][temp]
     incrK:
-            add     r14, 1                      ;Incrementing k counter
-            cmp     r14, rcx                    ;Checking if loop has finished (loops until k==n)
-            je      afterKLoop                  ;Coming back to jLoop
-            jmp     kLoop                       ;Coming back to loop
-
-
+            inc     r14                        ;Incrementing k counter
+            cmp     r14, rcx                   ;Checking if loop has finished (loops until k==n)
+            je      afterKLoop                 ;Coming back to jLoop
+            jmp     kLoop                      ;Coming back to loop
 
     endOfCalc:  
             ret
